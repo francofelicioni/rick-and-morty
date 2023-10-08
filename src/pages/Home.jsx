fa9e8f045e078144d19e748460d9dffe9d70cff6
Fran Felicioni <fran.felicioni@gmail.com>
2023-10-08 02:01:23 +0200

    fix: update layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { addAmount, decrement, increment } from '../features/counter/counterSlice';
+import { CharacterList } from '../components/CharacterList';
+import Pagination from '../components/pagination/Pagination';
+import Header from '../components/header/Header';
 
 
 const Home = () => {
-  return (
+  const [pageNumber, setPageNumber] = useState(1);
+  const [fetchedData, setFetchedData] = useState([]);
+  let { info, results } = fetchedData;
+  let apiUrl = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`
 
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(apiUrl).then(res => res.json())
+      setFetchedData(data)
+      console.log(pageNumber, apiUrl)
+    })()
+  }, [apiUrl, pageNumber])
+
+  return (
     <>
-      <h1 className="text-5xl font bold text-center py-2 bg-black text-white py-4">  LA CUARENTA GAME</h1>
+      <div className='bg-gradient-to-r from-purple-400 to-indigo-500'>
+        <Header />
+        <div className='container mx-auto'>
+          <Pagination setPageNumber={setPageNumber} pageNumber={pageNumber} />
+          <CharacterList characters={results} />
+          <Pagination setPageNumber={setPageNumber} pageNumber={pageNumber} />
+        </div>
+      </div>
     </>
   )
 }
