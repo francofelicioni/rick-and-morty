c530aaf0adaae87982a6f2a5e22700eaaa05a3bf
Fran Felicioni <fran.felicioni@gmail.com>
2023-10-08 02:00:41 +0200

    feat: create header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <h1 className="text-pink font-bold hover:text-light-salmon transition ease-in-out delay-150 text-3xl lg:text-5xl font bold text-center py-2 bg-black py-4">  RICK  <span className='text-green'>&</span> MORTY <span className='text-yellow'>WIKI</span> </h1>
+
+  )
+}
+
+export default Header
