4f9166a1ea91ac176eae296a0ef75c1e0340cf8b
Fran Felicioni <fran.felicioni@gmail.com>
2023-10-12 21:15:19 +0200

    fix: Update component

--- src/components/Filters/FilterButton.jsx
+++ src/components/Filters/FilterButton.jsx
@@ -1,37 +1,26 @@
 import React from 'react'
 
-const FilterButton = () => {
+const FilterButton = ({ selectedItem, handleSelect, item, name, index }) => {
+
+    let itemId = `${name}-${index}`;
+
     return (
         <div className='p-2'>
             <div className="flex items-center">
                 <input
-                    checked
-                    id="default-radio-1"
+                    id={itemId}
                     type="radio"
-                    name="default-radio"
-                    className="mx-2 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                    name={name}
+                    onChange={() => handleSelect(itemId)}
+                    className="mx-2 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 checked:bg-red-500 dark:border-gray-600"
                 />
-                <label 
-                    for="default-radio-1" 
-                    className="text-white bg-black hover:underline font-medium rounded-lg text-sm px-2.5 py-2"
+                <label
+                    htmlFor={itemId}
+                    className={`text-white bg-black hover:underline ${selectedItem === itemId ? 'underline' : ''} font-medium rounded-lg text-sm px-2.5 py-2`}
                 >
-                    Default radio
+                    {item}
                 </label>
-                
-            </div>
-            <div className="flex items-center">
-                <input
-                    id="default-radio-2"
-                    type="radio"
-                    name="default-radio"
-                    className="mx-2 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                />
-                <label 
-                    for="default-radio-2"
-                    className="text-white bg-black hover:underline font-medium rounded-lg text-sm px-2.5 py-2"
-                >
-                    Checked state
-                </label>
+
             </div>
         </div>
     )
