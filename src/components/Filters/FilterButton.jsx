6cd11acfed18ec1ee002e4e465f5690ba25e862d
Fran Felicioni <fran.felicioni@gmail.com>
2023-10-12 21:16:20 +0200

    fix: hide input

--- src/components/Filters/FilterButton.jsx
+++ src/components/Filters/FilterButton.jsx
@@ -12,7 +12,7 @@ const FilterButton = ({ selectedItem, handleSelect, item, name, index }) => {
                     type="radio"
                     name={name}
                     onChange={() => handleSelect(itemId)}
-                    className="mx-2 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 checked:bg-red-500 dark:border-gray-600"
+                    className="hidden"
                 />
                 <label
                     htmlFor={itemId}
