9ec8c370c6478b90b39b63b1a213cc9d2e89f110
Fran Felicioni <fran.felicioni@gmail.com>
2023-10-12 14:18:27 +0200

    fix: update functions

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactPaginate from 'react-paginate';
 
-const Pagination = ({info, setPageNumber, pageNumber }) => {
-
+const Pagination = ({ info, setPageNumber, pageNumber }) => {
 
     return (
         <div className='flex w-full justify-between p-4'>
@@ -12,11 +11,11 @@ const Pagination = ({info, setPageNumber, pageNumber }) => {
                 nextClassName="border border rounded-lg py-1 px-2 w-20 bg-blue-400 text-white text-center"
                 previousLabel="Previous"
                 previousClassName="border border rounded-lg py-1 px-2 w-20 bg-blue-400 text-white text-center"
-                onPageChange={(data)=>{setPageNumber(data.selected ++)}}
+                onPageChange={(data)=> {setPageNumber(data.selected +1)}}
                 pageRangeDisplayed={1}
                 pageCount={info?.pages}
-                forcePage={pageNumber === 1 ? 0 : pageNumber --}
-                className='flex font-bold justify-between items-center w-full lg:w-[50%] mx-auto'
+                forcePage={pageNumber === 1 ? 0 : pageNumber -1}
+                className='flex font-bold justify-between items-center w-full lg:w-[100%] mx-auto'
                 activeClassName='font-bold border w-6 h-6 text-center p-2.5 flex justify-center items-center rounded-full text-white'
             />
         </div>
