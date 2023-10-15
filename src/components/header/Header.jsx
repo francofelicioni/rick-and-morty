6bb4452270f30c43ad116515ea0c9fc8df6bf726
Fran Felicioni <fran.felicioni@gmail.com>
2023-10-15 20:02:33 +0200

    feat: add theme button switcher

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { MdDarkMode, MdOutlineDarkMode } from 'react-icons/md'
 
 const Header = () => {
 
@@ -23,7 +24,7 @@ const Header = () => {
       document.querySelector('html').classList.add('dark')
       localStorage.setItem('savedTheme', 'dark')
     } else {
-      document.querySelector('html').classList.remove('dark') 
+      document.querySelector('html').classList.remove('dark')
       localStorage.setItem('savedTheme', 'light')
     }
   }, [theme])
@@ -36,11 +37,27 @@ const Header = () => {
 
   return (
     <>
-      <h1 className="text-pink font-bold hover:text-light-salmon transition ease-in-out delay-150 text-3xl lg:text-5xl font bold text-center py-2 bg-black py-4">  RICK  <span className='text-green'>&</span> MORTY <span className='text-yellow'>WIKI</span>
-      </h1>
-      <button className='text-black' onClick={handleClick}>
-        Change Theme
-      </button>
+      <div className='flex justify-center items-center py-2 dark:bg-black'>
+        <h1 className="text-pink font-bold hover:text-light-salmon text-3xl lg:text-5xl">  RICK  <span className='text-green'>&</span> MORTY <span className='text-yellow'>WIKI</span></h1>
+
+        <div className='absolute right-0 p-5 flex items-center' onClick={handleClick}>
+          {theme === 'dark' ? (
+            <>
+              <button className='w-10 h-10 text-white' >
+                <MdOutlineDarkMode />
+              </button>
+              <span className='text-white'>Light</span>
+            </>
+          ) : (
+            <>
+              <button className='w-10 h-10 text-white' >
+                <MdDarkMode />
+              </button>
+              <span className='text-white'>Dark</span>
+            </>
+          )}
+        </div>
+      </div>
     </>
 
   )
