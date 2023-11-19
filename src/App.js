3aa5fd3563368105c374ad53f2f5a2cb35209c2a
“francofelicioni” <fran.felicioni@gmail.com>
2023-11-19 20:03:19 +0100

    fix: add new routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Locations from './pages/Locations';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
+import CardDetails from './components/Cards/CardDetails';
 
 function App() {
   return (
@@ -14,8 +15,11 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/:id' element={<CardDetails />} />
           <Route path='/episodes' element={<Episodes />} />
+          <Route path='/episodes/:id' element={<CardDetails />} />
           <Route path='/locations' element={<Locations />} />
+          <Route path='/locations/:id' element={<CardDetails />} />
         </Routes>
       </Router>
     </>
