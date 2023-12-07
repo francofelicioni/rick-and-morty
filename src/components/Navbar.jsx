17b7bf940b478205b330b854ea7931abb8c64547
“francofelicioni” <fran.felicioni@gmail.com>
2023-12-07 15:26:36 -0300

    fix: update layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <div className='flex justify-center items-center gap-x-4 dark:bg-black dark:text-white py-2'>
-            <Link className='cursor-pointer active:underline' to="/">Characters</Link>
+        <div className='flex justify-center items-center gap-x-4 bg-yellow dark:bg-slate-800 dark:text-white py-3'>
+            <Link className='cursor-pointer font-semibold active:underline' to="/">Characters</Link>
             <span>•</span>
-            <Link className='cursor-pointer active:underline' to="/episodes">Episodes</Link>
+            <Link className='cursor-pointer font-semibold active:underline' to="/episodes">Episodes</Link>
             <span>•</span>
-            <Link className='cursor-pointer  active:underline' to="/locations">Locations</Link>
+            <Link className='cursor-pointer font-semibold  active:underline' to="/locations">Locations</Link>
         </div>
     )
 }
