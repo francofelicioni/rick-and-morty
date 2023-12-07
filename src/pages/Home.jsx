4da74f0f5cbcd09e7f562599f0b5dbd341b31b5a
“francofelicioni” <fran.felicioni@gmail.com>
2023-12-07 15:27:21 -0300

    fix: update layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
 
   return (
     <>
-      <div className='bg-gradient-to-r from-indigo-500 dark:from-indigo-900  to-purplepink-500 dark:to-slate-800 h-full'>
+      <div className='bg-gradient-to-r from-red-400 dark:from-indigo-900  to-light-blue dark:to-yellow h-full'>
         <div className='container mx-auto pt-4'>
           <SearchBar searchInput={searchInput} setSearchInput={setSearchInput} />
           <div className='flex justify-between px-5'>
