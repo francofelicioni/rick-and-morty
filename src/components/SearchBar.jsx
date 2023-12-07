7316680303df1b0c210a655611486f5c46d58ebe
“francofelicioni” <fran.felicioni@gmail.com>
2023-12-07 18:04:00 -0300

    fix: add border color

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,8 +5,8 @@ const SearchBar = ({searchInput, setSearchInput}) => {
 
     return (
         <form className='flex justify-between gap-2 w-full px-5 mx-auto'>
-            <input onKeyUp={e => setSearchInput(e.target.value) } className="w-full text-xl pl-2 h-12 rounded-lg mx-auto shadow-lg" placeholder='Entrer a character name' autoFocus type="text" />
-            <button onClick={e => e.preventDefault()} className='bg-yellow text-unknown uppercase cursor-pointer font-bold rounded-lg border border-1 w-36 shadow-lg'>Search</button>
+            <input onKeyUp={e => setSearchInput(e.target.value) } className="w-full text-xl pl-2 h-12 rounded-lg mx-auto shadow-lg dark:border-black" placeholder='Entrer a character name' autoFocus type="text" />
+            <button onClick={e => e.preventDefault()} className='bg-yellow text-unknown uppercase cursor-pointer font-bold rounded-lg border border-1 dark:border-black w-36 shadow-lg'>Search</button>
         </form>
     )
 }
